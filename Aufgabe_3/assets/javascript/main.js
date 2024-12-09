bc50d8f0d41599bf00f3ab8a5dea6e72a6a960f9
YvonneJL <yvonneloder@gmx.de>
2024-12-09 14:57:30 +0100

    fertig

---
+++ Aufgabe_3/assets/javascript/main.js
@@ -0,0 +1,10 @@
+let x = 34;
+
+x = 67;
+console.log(x);
+
+const y = 34;
+
+y = 67;
+
+// !Funktioniert nicht, weil sich const nicht überschreiben lässt
---
+++ Aufgabe_4/assets/javascript/main.js
@@ -0,0 +1,20 @@
+let helloWorld = "Hello World";
+console.log(helloWorld);
+
+const pi = 3.14;
+console.log(pi);
+
+let burjDubai = "828 m"
+console.log(burjDubai);
+
+const fullName = "Jan Schmidt"
+console.log(fullName);
+
+const eifelTower = "324 m";
+console.log(eifelTower);
+
+const camelCase = "camel Case"
+console.log(camelCase);
+
+let kursStatus = "true";
+console.log(kursStatus);
